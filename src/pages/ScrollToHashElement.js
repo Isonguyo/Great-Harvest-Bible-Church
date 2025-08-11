1c185983a6ac610e7acfefcf1ef97b5a0064713f
Isonguyo <princewillisonguyo@gmail.com>
2025-08-11 10:32:24 +0100

    Create ScrollToHashElement.js
    new function

---
+++ src/pages/ScrollToHashElement.js
@@ -0,0 +1,21 @@
+// src/components/ScrollToHashElement.js
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToHashElement() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      // Delay so that content is fully loaded before scrolling
+      setTimeout(() => {
+        const el = document.querySelector(hash);
+        if (el) {
+          el.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+      }, 200);
+    }
+  }, [hash]);
+
+  return null;
+}
