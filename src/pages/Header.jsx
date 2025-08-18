e1fa27e6458d5e09e60c4721102c1f9e2165be02
Princewill Isonguyo <princewillisonguyo@gmail.com>
2025-08-18 13:03:49 +0100

    Update Header.jsx
    nav update

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -22,7 +22,6 @@ const Header = () => {
   const handleNavClick = (to) => {
     const hash = to.includes('#') ? to.split('#')[1] : '';
     if (location.pathname === "/" && location.hash === `#${hash}`) {
-      // Already at the correct hash on home page, manually scroll
       const el = document.getElementById(hash);
       if (el) scrollToElement(el);
     } else {
@@ -48,10 +47,10 @@ const Header = () => {
         <ul className="nav-links">
           <li><Link smooth to="/#home" scroll={scrollToElement}>Home</Link></li>
           <li><Link smooth to="/about" scroll={scrollToElement}>About</Link></li>
-          <li><Link smooth to="/#sermons" scroll={scrollToElement}>Sermons</Link></li>
-          <li><Link smooth to="/#ministries" scroll={scrollToElement}>Ministries</Link></li>
-          <li><Link smooth to="/#events" scroll={scrollToElement}>Events</Link></li>
-          <li><Link smooth to="/#contact" scroll={scrollToElement}>Contact</Link></li>
+          <li><Link smooth to="/children-church" scroll={scrollToElement}>Children</Link></li>
+          <li><Link smooth to="/Youth" scroll={scrollToElement}>Youth</Link></li>
+          <li><Link smooth to="/adult" scroll={scrollToElement}>Adult</Link></li>
+          <li><Link smooth to="/worship" scroll={scrollToElement}>Worship</Link></li>
         </ul>
 
         {/* Give Button */}
@@ -71,27 +70,28 @@ const Header = () => {
         </button>
       </div>
 
-    {/* Mobile Menu */}
-<div className={`mobile-menu ${menuOpen ? "show" : ""}`}> 
-  {location.pathname === "/" ? (
-    <>
-      <Link smooth to="/#home" scroll={scrollToElement} onClick={() => handleNavClick("/#home")}>Home</Link>
-      <Link smooth to="/about" scroll={scrollToElement} onClick={() => handleNavClick("/about")}>About</Link>
-      <Link smooth to="/#sermons" scroll={scrollToElement} onClick={() => handleNavClick("/#sermons")}>Sermons</Link>
-      <Link smooth to="/#ministries" scroll={scrollToElement} onClick={() => handleNavClick("/#ministries")}>Ministries</Link>
-      <Link smooth to="/#events" scroll={scrollToElement} onClick={() => handleNavClick("/#events")}>Events</Link>
-      <Link smooth to="/#contact" scroll={scrollToElement} onClick={() => handleNavClick("/#contact")}>Contact</Link>
-      <Link className="mobile-give" smooth to="/#give" scroll={scrollToElement} onClick={() => handleNavClick("/#give")}>Give Online</Link>
-    </>
-  ) : (
-    <>
-      <Link smooth to="/#home" scroll={scrollToElement} onClick={() => handleNavClick("/#home")}>Home</Link>
-      <Link smooth to="/about" scroll={scrollToElement} onClick={() => handleNavClick("/about")}>About</Link>
-    </>
-  )}
-</div>
+      {/* Mobile Menu */}
+      <div className={`mobile-menu ${menuOpen ? "show" : ""}`}>
+        {location.pathname === "/" ? (
+          <>
+            <Link smooth to="/#home" scroll={scrollToElement} onClick={() => handleNavClick("/#home")}>Home</Link>
+            <Link smooth to="/about" scroll={scrollToElement} onClick={() => handleNavClick("/about")}>About</Link>
+            <Link smooth to="/children-church" scroll={scrollToElement} onClick={() => handleNavClick("/#children")}>Children</Link>
+            <Link smooth to="/Youth" scroll={scrollToElement} onClick={() => handleNavClick("/#youth")}>Youth</Link>
+            <Link smooth to="/adult" scroll={scrollToElement} onClick={() => handleNavClick("/#adult")}>Adult</Link>
+            <Link smooth to="/worship" scroll={scrollToElement} onClick={() => handleNavClick("/#worship")}>Worship</Link>
+            <Link className="mobile-give" smooth to="/#give" scroll={scrollToElement} onClick={() => handleNavClick("/#give")}>Give Online</Link>
+          </>
+        ) : (
+          <>
+            <Link smooth to="/#home" scroll={scrollToElement} onClick={() => handleNavClick("/#home")}>Home</Link>
+            <Link smooth to="/about" scroll={scrollToElement} onClick={() => handleNavClick("/about")}>About</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
 
 export default Header;
+
